18f438473eb53954e112965cae1dac748b236404
heenabarve <mogalheena1992@gmail.com>
2021-06-15 07:09:31 -0700

    My Employee Database Test Project

---
+++ src/main/java/com/heena/database/demo/EmployeeDb.java
@@ -0,0 +1,81 @@
+package com.heena.database.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+public class EmployeeDb 
+{
+	Connection con;
+	
+	public EmployeeDb(String url,String userName,String password)
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			con= DriverManager.getConnection( url, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void closeConnection()
+	{
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public HashMap getEmployeeById(int id) throws SQLException
+	{
+		HashMap<String,String> map = new HashMap<String,String>();
+		
+		Statement stmt = con.createStatement();
+		
+		ResultSet rs = stmt.executeQuery("select * from employees where emp_id="+id);
+		
+		while(rs.next())
+		{
+			
+			map.put("emp_id", rs.getInt(1)+"");
+			map.put("first_name", rs.getNString(2));
+			//map.put("salary", rs.getInt(3));
+				
+		}
+		return map;
+	}
+	
+	
+	public HashMap getEmployeeByName(String name) throws SQLException
+	{
+		HashMap<String,String> map = new HashMap<String,String>();
+		
+		Statement stmt = con.createStatement();
+		
+		ResultSet rs = stmt.executeQuery("select * from employees where first_name='"+name+"'");
+		
+		while(rs.next())
+		{
+			
+			map.put("emp_id", rs.getInt(1)+"");
+			map.put("first_name", rs.getNString(2));
+			//map.put("salary", rs.getInt(3));
+				
+		}
+		return map;
+	}
+	
+}
---
+++ src/test/java/com/heena/database/demo/EmployeeDBTest.java
@@ -0,0 +1,69 @@
+package com.heena.database.demo;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import javax.management.DescriptorKey;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class EmployeeDBTest 
+
+{
+
+
+	static EmployeeDb db = null;
+
+	@BeforeClass
+	public static void creteConnection()
+	{
+		System.out.println("Inside before class !!!");
+		String url="jdbc:mysql://localhost/heena";
+		String userName= "abhi";
+		String password= "abhi";
+		db = new EmployeeDb(url,userName,password);
+	}
+
+	@AfterClass
+	public static void closeConnection()
+	{
+		System.out.println("Inside after class !!!");
+		db.closeConnection();
+	}
+
+	@Test
+	public void getEmployeeByIdTest() throws SQLException
+	{
+		System.out.println("Inside getEmployeeByIdTest !!!");
+		
+		HashMap<String, String> result = db.getEmployeeById(1);
+
+		System.out.println("emp_id="+result.get("emp_id"));
+		System.out.println("first_name="+result.get("first_name"));
+
+		Assert.assertEquals(result.size(), 2);
+	}
+
+
+	@Test
+	public void getEmployeeByName() throws SQLException
+	{
+		System.out.println("Inside getEmployeeByName !!!");
+		
+		HashMap<String, String> result = db.getEmployeeByName("Abhi");
+
+		System.out.println("emp_id="+result.get("emp_id"));
+		System.out.println("first_name="+result.get("first_name"));
+
+		Assert.assertEquals(result.size(), 2);
+	}
+}
